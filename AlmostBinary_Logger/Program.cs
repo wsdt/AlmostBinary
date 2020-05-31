dbddb7c38fe29acbd5d28959582fbc02a54d3525
wsdt <kevin.riedl.privat@gmail.com>
2020-05-31 17:35:27 +0200

    variable.abin works

---
+++ AlmostBinary_Logger/Program.cs
@@ -0,0 +1,98 @@
+﻿using AlmostBinary_GlobalConstants;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.IO;
+
+namespace AlmostBinary_Logger
+{
+    public class Program
+    {
+        /// <summary>
+        /// Used to bring at least some context to the Program class, otherwise most context fields in outputTemplate would be empty.
+        /// Line-No, method name .. still empty.
+        /// </summary>
+        private static ILogger StartupLogger => Serilog.Log.ForContext<Program>();
+
+        #region methods
+        public static void Main(string[] args)
+        {
+
+            IServiceCollection services = ConfigureServices();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetService<Startup>().Run(args);
+
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnShutdown!);
+        }
+
+        /// <summary>
+        /// Is executed on proccess exit.
+        /// </summary>
+        private static void OnShutdown(object sender, EventArgs e)
+        {
+            StartupLogger.Information("Quitting compiler.");
+            Log.CloseAndFlush();
+        }
+
+        /// <summary>
+        /// Configures global services.
+        /// </summary>
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfiguration logConfiguration = BuildConfiguration();
+            services.AddSingleton(logConfiguration);
+
+            // Required to run application
+            services.AddTransient<Startup>();
+
+            Log.Logger = CraftLogger(logConfiguration);
+
+            StartupLogger.Information("Attached and configured services.");
+            return services;
+        }
+
+        /// <summary>
+        /// Makes configuration accessible related to current build (debug/release).
+        /// </summary>
+        /// <returns>Global Configuration</returns>
+        private static IConfiguration BuildConfiguration() => new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(IGlobalConstants.PROJECT_ROOT_PATH, "Properties"))
+                .AddJsonFile(IGlobalConstants.APP_SETTINGS_FILE, optional: false, reloadOnChange: true)
+                .Build();
+
+        /// <summary>
+        /// Builds File- and Console logger
+        /// </summary>
+        /// <param name="configuration">Global configuration</param>
+        /// <returns>Logger</returns>
+        private static ILogger CraftLogger(IConfiguration configuration)
+        {
+            string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} - {SourceContext}:{MemberName}:{LineNumber}{NewLine}{Exception}";
+            LogEventLevel logLevel = configuration.GetValue<LogEventLevel>("Runtime:Logging:LogLevel");
+
+            return new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .MinimumLevel.Verbose()
+            .WriteTo.Async(f => f.File(
+                Path.Combine(
+                    IGlobalConstants.PROJECT_ROOT_PATH,
+                    configuration.GetValue<string>("Runtime:Logging:LogOutputPath")
+                ),
+                rollingInterval: RollingInterval.Day,
+                buffered: true,
+                restrictedToMinimumLevel: logLevel,
+                outputTemplate: outputTemplate))
+            .WriteTo.Console(
+                outputTemplate: outputTemplate,
+                restrictedToMinimumLevel: logLevel,
+                theme: AnsiConsoleTheme.Code
+                )
+            .CreateLogger();
+        }
+    }
+    #endregion
+}
---
+++ AlmostBinary_Logger/Startup.cs
@@ -0,0 +1,19 @@
+﻿using AlmostBinary_Logger.utils;
+using Serilog;
+
+namespace AlmostBinary_Logger
+{
+    public class Startup
+    {
+        #region fields
+        private static ILogger Log => Serilog.Log.ForContext<Startup>();
+        #endregion
+
+        #region methods
+        public void Run(string[] args)
+        {
+            Log.Here().Verbose("Started logger service.");
+        }
+        #endregion
+    }
+}
