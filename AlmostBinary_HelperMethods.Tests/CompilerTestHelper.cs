302c95a635e8b03452440891aef9d550dc17639a
wsdt <kevin.riedl.privat@gmail.com>
2020-06-07 15:49:41 +0200

    testwise

--- AlmostBinary_HelperMethods.Tests/CompilerTestHelper.cs
+++ AlmostBinary_HelperMethods.Tests/CompilerTestHelper.cs
@@ -1,5 +1,6 @@
 ﻿using AlmostBinary_GlobalConstants.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics;
 using System.IO;
 
@@ -19,6 +20,9 @@ namespace AlmostBinary_HelperMethods.Tests
         /// <param name="fileName">File name without file-extension.</param>
         public static void Compile(string fileName)
         {
+            Console.WriteLine("##### " + IGlobalTestConstants.COMPILER_EXE_PATH+ " / "+File.Exists(IGlobalTestConstants.COMPILER_EXE_PATH)+ " / "
+                +Path.Combine(IGlobalTestConstants.EXAMPLES_PATH, $"{fileName}.{IGlobalTestConstants.UNCOMPILED_FILE_TYPE}")+ " / "+File.Exists(Path.Combine(IGlobalTestConstants.EXAMPLES_PATH, $"{fileName}.{IGlobalTestConstants.UNCOMPILED_FILE_TYPE}")));
+
             Process compiler = Process.Start(IGlobalTestConstants.COMPILER_EXE_PATH,
                 Path.Combine(IGlobalTestConstants.EXAMPLES_PATH, $"{fileName}.{IGlobalTestConstants.UNCOMPILED_FILE_TYPE}"));
             compiler.CloseMainWindow();
